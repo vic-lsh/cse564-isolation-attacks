ad9a92502c9419d9f6cc355f02b22440e1b8e0d6
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-03-04 19:23:06 +0000

    data: add server ip parsing logic

--- firecracker/data/src/netbw_vm.c
+++ firecracker/data/src/netbw_vm.c
@@ -9,8 +9,10 @@
 #include <time.h>
 #include <sys/time.h>
 #include <signal.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <netdb.h>
 
-#define SERVER_IP "172.16.0.1"
 #define SERVER_PORT 5001  // Traditional iperf port
 #define BUFFER_SIZE 65536  // 64KB buffer size
 #define TEST_DURATION 10   // Default test duration in seconds
@@ -31,6 +33,50 @@ double elapsed_time_sec(struct timeval *start, struct timeval *end) {
            (end->tv_usec - start->tv_usec) / 1000000.0;
 }
 
+// Function to determine server IP based on eth0 IP
+char* get_server_ip() {
+    struct ifaddrs *ifaddr, *ifa;
+    static char server_ip[INET_ADDRSTRLEN];
+    char host[NI_MAXHOST];
+    
+    // Get list of network interfaces
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        return NULL;
+    }
+    
+    // Find eth0 interface with IPv4 address
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+            
+        // Only interested in eth0 and IPv4
+        if (strcmp(ifa->ifa_name, "eth0") == 0 && 
+            ifa->ifa_addr->sa_family == AF_INET) {
+            
+            // Convert network address to string
+            struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+            inet_ntop(AF_INET, &(sa->sin_addr), host, NI_MAXHOST);
+            
+            // Copy the IP address to modify
+            strncpy(server_ip, host, INET_ADDRSTRLEN);
+            
+            // Extract the last digit and subtract one
+            char *last_octet = strrchr(server_ip, '.') + 1;
+            int octet_value = atoi(last_octet);
+            octet_value--;
+            
+            // Replace the last octet
+            sprintf(last_octet, "%d", octet_value);
+            
+            break;
+        }
+    }
+    
+    freeifaddrs(ifaddr);
+    return server_ip;
+}
+
 int main(int argc, char *argv[]) {
     int sockfd;
     struct sockaddr_in server_addr;
@@ -40,6 +86,7 @@ int main(int argc, char *argv[]) {
     struct timeval start_time, current_time;
     double elapsed_secs;
     int duration = TEST_DURATION;
+    char *server_ip;
     
     // Check for custom duration
     if (argc > 1) {
@@ -53,6 +100,15 @@ int main(int argc, char *argv[]) {
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
     
+    // Get server IP based on eth0
+    server_ip = get_server_ip();
+    if (server_ip == NULL) {
+        fprintf(stderr, "Failed to determine server IP from eth0. Please check your network configuration.\n");
+        return 1;
+    }
+    
+    printf("Determined server IP: %s\n", server_ip);
+    
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -65,17 +121,17 @@ int main(int argc, char *argv[]) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
         perror("Invalid address");
         close(sockfd);
         return 1;
     }
     
     // Connect to server
-    printf("Attempting to connect to %s:%d...\n", SERVER_IP, SERVER_PORT);
+    printf("Attempting to connect to %s:%d...\n", server_ip, SERVER_PORT);
     if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Connection failed");
-        printf("Make sure you have a server listening at %s:%d\n", SERVER_IP, SERVER_PORT);
+        printf("Make sure you have a server listening at %s:%d\n", server_ip, SERVER_PORT);
         close(sockfd);
         return 1;
     }
