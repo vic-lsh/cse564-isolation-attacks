89acda8b8dc4367997514dc7552b9a253dfc49e8
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-03-04 04:49:00 +0000

    script: add script to download firecracker

---
+++ firecracker/download_firecracker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ARCH="$(uname -m)"
+release_url="https://github.com/firecracker-microvm/firecracker/releases"
+latest=$(basename $(curl -fsSLI -o /dev/null -w  %{url_effective} ${release_url}/latest))
+curl -L ${release_url}/download/${latest}/firecracker-${latest}-${ARCH}.tgz \
+| tar -xz
+
+# Rename the binary to "firecracker"
+mv release-${latest}-$(uname -m)/firecracker-${latest}-${ARCH} firecracker
